973b586d3422483b1e96263c8584cb8302ac0332
Anton Dobrynin <a.dobrynin@amarkets.com>
2024-05-13 11:51:03 +0700

    init

---
+++ Pictoria/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pictoria
+//
+//  Created by ANTON DOBRYNIN on 13.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pictoria/PictoriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PictoriaApp.swift
+//  Pictoria
+//
+//  Created by ANTON DOBRYNIN on 13.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct PictoriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
